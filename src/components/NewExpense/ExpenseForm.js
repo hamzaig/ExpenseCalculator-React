a9dab78f49ec8ecc9285aa6041dd9e3d1466c505
hamzaig <hamzaig@yahoo.com>
2021-10-06 13:45:32 +0500

    Working with Multiple States

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+    const [inputTitle, setInputTitle] = useState("");
+    const [inputAmount, setInputAmount] = useState("");
+    const [inputDate, setInputDate] = useState("");
+    console.log(inputTitle, inputAmount, inputDate);
     return (
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={(e) => (console.log(e.target.value))} />
+                    <input type="text" onChange={(e) => (setInputTitle(e.target.value))} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" />
+                    <input type="number" min="0.01" step="0.01" onChange={(e) => (setInputAmount(e.target.value))} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2021-10-01" max="2023-10-01" />
+                    <input type="date" min="2021-10-01" max="2023-10-01" onChange={(e) => (setInputDate(e.target.value))} />
                 </div>
             </div>
             <div className="new-expense__actions">
