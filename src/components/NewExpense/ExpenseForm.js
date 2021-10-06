18005e104785b96c593468441ca04a1d9e2ec2a4
hamzaig <hamzaig@yahoo.com>
2021-10-06 14:37:18 +0500

    Updating State That Depends On The Previous State

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -18,24 +18,24 @@ const ExpenseForm = () => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={(e) => (setUserInput({
-                        ...userInput,
+                    <input type="text" onChange={(e) => (setUserInput((previousValues) => ({
+                        ...previousValues,
                         inputTitle: e.target.value,
-                    }))} />
+                    })))} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={(e) => (setUserInput({
-                        ...userInput,
+                    <input type="number" min="0.01" step="0.01" onChange={(e) => (setUserInput((previousValues) => ({
+                        ...previousValues,
                         inputAmount: e.target.value,
-                    }))} />
+                    })))} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2021-10-01" max="2023-10-01" onChange={(e) => (setUserInput({
-                        ...userInput,
+                    <input type="date" min="2021-10-01" max="2023-10-01" onChange={(e) => (setUserInput((previousValues) => ({
+                        ...previousValues,
                         inputDate: e.target.value,
-                    }))} />
+                    })))} />
                 </div>
             </div>
             <div className="new-expense__actions">
