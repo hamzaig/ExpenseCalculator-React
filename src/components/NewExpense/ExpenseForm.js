5faf1683d5da3955abe7cdd802416047ea6d5422
hamzaig <hamzaig@yahoo.com>
2021-10-10 03:37:17 +0500

    Adding Two-Way Binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -22,7 +22,13 @@ const ExpenseForm = () => {
         }
 
         console.log(expenseDate);
-
+        setUserInput({
+            inputTitle: "",
+            inputAmount: "",
+            inputDate: "",
+        });
+        // set = "";
+        // set = "";
     }
 
     return (
@@ -30,21 +36,21 @@ const ExpenseForm = () => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={(e) => (setUserInput((previousValues) => ({
+                    <input type="text" value={userInput.inputTitle} onChange={(e) => (setUserInput((previousValues) => ({
                         ...previousValues,
                         inputTitle: e.target.value,
                     })))} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={(e) => (setUserInput((previousValues) => ({
+                    <input type="number" min="0.01" step="0.01" value={userInput.inputAmount} onChange={(e) => (setUserInput((previousValues) => ({
                         ...previousValues,
                         inputAmount: e.target.value,
                     })))} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2021-10-01" max="2023-10-01" onChange={(e) => (setUserInput((previousValues) => ({
+                    <input type="date" min="2021-10-01" max="2023-10-01" value={userInput.inputDate} onChange={(e) => (setUserInput((previousValues) => ({
                         ...previousValues,
                         inputDate: e.target.value,
                     })))} />
