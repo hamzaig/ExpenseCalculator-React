bb7e4c87fa7e6f6d2ecde71648e6b60b44bea7bf
hamzaig <hamzaig@yahoo.com>
2021-10-16 21:56:02 +0500

    Outputting Conditional Content

--- src/App.js
+++ src/App.js
@@ -8,25 +8,25 @@ const DUUMMY_EXPENSE = [
     id: 0,
     title: "Car Insurrance",
     amount: 5646,
-    date: new Date(2014, 2, 21),
+    date: new Date(2020, 2, 21),
   },
   {
     id: 1,
     title: "Gas",
     amount: 465,
-    date: new Date(2014, 2, 22),
+    date: new Date(2021, 2, 22),
   },
   {
     id: 2,
     title: "Electricity",
     amount: 54,
-    date: new Date(2014, 2, 23),
+    date: new Date(2019, 2, 23),
   },
   {
     id: 3,
     title: "Mobile Bill",
     amount: 465,
-    date: new Date(2014, 2, 30),
+    date: new Date(2019, 2, 30),
   },
 ]
 
--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -10,13 +10,22 @@ const Expenses = (props) => {
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
     };
+    const filteredExpenses = props.expense.filter(exp => {
+        return exp.date.getFullYear().toString() === filteredYear;
+    })
+
+    let expensesContent = <p>There Is Noting on That Date</p>;
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map(expense => (<ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date} />));
+    }
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            {props.expense.map(expense => (<ExpenseItem
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date} />))}
+            {expensesContent}
         </Card>
     );
 }
